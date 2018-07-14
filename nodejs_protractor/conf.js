99da4a11b8970c4d411ef912acfa51f1ad4e5742
STPAG\Bernd.Wissmeier <bernd.wissmeier@stp-online.de>
2018-07-15 00:01:17 +0200

    add git

---
+++ nodejs_protractor/conf.js
@@ -0,0 +1,36 @@
+﻿var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+
+exports.config = {
+    // The address of a running selenium server. 
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    // Capabilities to be passed to the webdriver instance. 
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    // Spec patterns are relative to the current working directly when 
+    // protractor is called. 
+    specs: ['example_spec.js'],
+
+    // Options to be passed to Jasmine-node. 
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    
+    },
+
+    onPrepare: function () {
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: './test/reports',
+                fileName: "test",
+                fileNameDateSuffix: true,
+                takeScreenshotsOnlyOnFailures: true,
+                cleanDestination: false
+                //git test
+            })
+        );
+    }
+};
